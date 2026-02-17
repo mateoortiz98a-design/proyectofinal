7a4cc625be0585247c2084565aa2973c19ea59cb
mateo.dev <mateoortiz98a@gmail.com>
2026-02-17 19:47:36 -0300

    proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomeScreen from './Screens/HomeScreen/HomeScreen'
+import ContactScreen from './Screens/ContactScreen/ContactScreen'
+import ErrorNotFoundScreen from './Screens/ErrorNotFoundScreen/ErrorNotFoundScreen'
+import { getContacts } from './services/contactsService'
+import ContactsContextProvider from './Context/ContactsContext'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            <ContactsContextProvider >
+                <Routes>
+                    <Route
+                        path='/'
+                        element={
+
+                            <HomeScreen  />
+
+                        }
+                    />
+                    <Route
+                        path='/contact/:contact_id'
+                        element={
+                            <ContactScreen />
+                        }
+                    />
+                    <Route
+                        path='*'
+                        element={<ErrorNotFoundScreen />}
+                    />
+                </Routes>
+            </ContactsContextProvider>
+        </div>
+    )
 }
 
 export default App
